4c0b6c94f6f2615a104f8015c730c624a1988a78
Mmesoma Saint <mmesomasaint@gmail.com>
2024-11-11 17:28:54 +0100

    Fix: Remove getServerSideProps

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,15 @@
 import AuthForm from '@/components/authForm'
-import { NextApiRequest } from 'next/types'
+import {cookies} from 'next/headers'
 
-export default function Home({code}: {code: string}) {
+export default async function Home() {
+  const cookieStore = await cookies()
+  const code = cookieStore.get('x-code')
+
+  // Move to next auth step if code is present
 
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-100'>
       <AuthForm />
     </div>
   )
-}
-
-export async function getServerSideProps({ req }: {req: NextApiRequest} ) {
-  // Get the 'x-code' from the cookies
-  const code = req.cookies['x-code']
-
-  // If there's no 'x-code', redirect or render a default message
-  if (!code) {
-    return {
-      props: { code: null },
-    }
-  }
-
-  return {
-    props: { code }, // Pass the 'x-code' to the page component
-  }
 }
